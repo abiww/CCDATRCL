e4724d52b32a3ea0d3b8f4a43064bf4e9b0bbc6f
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-08-30 17:21:27 +0800

    Create Palindrome.java

---
+++ Mental WarmUP/Palindrome.java
@@ -0,0 +1,33 @@
+/* Mental - WarmUP #3
+ * Identifying if a string is Palindrome or not
+ * Edit the program; add statement to count the number of characters in the entered string.
+ */
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+    	System.out.print("Enter String: ");
+    	Scanner scan = new Scanner(System.in);
+    	String str = scan.nextLine();
+    	
+    	int count = str.length();
+    	char[] ch = str.toCharArray();
+    	int num = 0;
+		
+		for(int i = 0; i < ch.length/2; i++) {
+			if(ch[i] != ch[ch.length-1-i]) {
+				num = 1; break;
+			}
+		}
+		
+		if(num == 0) {
+			System.out.println("The word " + str + " is Palindrome");
+			System.out.println("Count: " + count + " characters");
+		} else {
+			System.out.println("The word " + str + " is not Palindrome");
+			System.out.println("Count: " + count + " characters");
+		}
+  }
+}
+
